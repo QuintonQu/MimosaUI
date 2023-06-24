abb61098c6982781007858e1dffb91b7c6ab9887
QuintonQu <58494325+QuintonQu@users.noreply.github.com>
2023-06-24 16:20:15 -0400

    Initial Commit

---
+++ MimosaUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MimosaUI
+//
+//  Created by Ziyuan Qu on 2023/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MimosaUI/MimosaUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MimosaUIApp.swift
+//  MimosaUI
+//
+//  Created by Ziyuan Qu on 2023/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MimosaUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
